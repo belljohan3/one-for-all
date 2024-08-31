4e4c517deb2bd2302e26175ea1ae3a04f8a3a1f4
Johan Bell <belljohan3@gmail.com>
2024-08-31 12:00:56 +0100

    install dependencies && add config

---
+++ backend/jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from '@jest/types';
+
+// Jest configuration
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+};
+
+export default config;
